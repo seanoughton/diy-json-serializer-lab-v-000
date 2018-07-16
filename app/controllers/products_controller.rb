03ecef666209d07e13e8b9b2e75b2db839decebf
Sean Oughton <seanoughton@gmail.com>
2018-07-16 22:30:56 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,10 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def show
+    product = Product.find(params[:id])
+  end
+
   def inventory
     product = Product.find(params[:id])
     render plain: product.inventory > 0 ? true : false
@@ -10,6 +14,7 @@ class ProductsController < ApplicationController
 
   def data
     product = Product.find(params[:id])
+    render json: ProductSerializer.serialize(product)
   end
 
   def description
