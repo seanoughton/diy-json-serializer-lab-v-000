b07589fd63b96202f12b9ac5ce39def37826662b
Sean Oughton <seanoughton@gmail.com>
2018-07-16 22:48:50 +0000

    Done.

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -4,12 +4,12 @@ class ProductSerializer
     # start with the open brace to create a valid JSON object
     serialized_product = '{'
 
-    serialized_product += '"name": ' + product.name + ', '
-    serialized_product += '"price": "' + product.price.to_s + '", '
-    serialized_product += '"inventory": "' + product.inventory + '", '
-    serialized_product += '"description": "' + product.description + '"}'
+    serialized_product += '"id": ' + product.id.to_s + ', '
+    serialized_product += '"name": "' + product.name + '", '
+    serialized_product += '"description": "' + product.description + '", '
+    serialized_product += '"inventory": ' + product.inventory.to_s + ', '
+    serialized_product += '"price": "' + product.price.to_s + '"'
 
-    # and end with the close brace
     serialized_product += '}'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
-  resources :products, only: [:index, :new, :create, :show]
+  resources :products #, only: [:index, :new, :create, :show]
   get '/products/:id/description', to: 'products#description'
   get '/products/:id/inventory', to: 'products#inventory'
   get 'products/:id/data', to: 'products#data'
+  get 'products/:id/show', to: 'products#show'
 end
